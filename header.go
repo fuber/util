13192b1a2115f58a8f00c6e77d489dfafb162e6e
FuberDev <pwnfactory+fuber@gmail.com>
2014-11-29 13:16:09 +0100

    initial commit

---
+++ header.go
@@ -0,0 +1,18 @@
+package util
+
+import (
+	"fmt"
+	"time"
+)
+
+const TimeFormat = "Mon, 02 Jan 2006 15:04:05 GMT"
+const HTTPHeader = `HTTP/1.1 200 OK
+Date: %s
+Content-Type: text/plain; charset=UTF-8
+Transfer-Encoding: chunked
+
+`
+
+func ChunkedHeader() string {
+	return fmt.Sprintf(HTTPHeader, time.Now().UTC().Format(TimeFormat))
+}
